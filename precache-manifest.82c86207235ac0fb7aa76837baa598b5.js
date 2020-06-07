55568069837f0ae033ed93fa8ea9cb1721605468
farouk877 <faroukmarhaba1@gmail.com>
2020-06-07 14:15:26 -0400

    Updates

---
+++ precache-manifest.82c86207235ac0fb7aa76837baa598b5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c961fd1d7dfba3893531ef57e3dff4bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd17164976da859ed677",
+    "url": "/static/css/2.4deadad5.chunk.css"
+  },
+  {
+    "revision": "73ccbb36162e82462925",
+    "url": "/static/css/main.8c2f8d65.chunk.css"
+  },
+  {
+    "revision": "fd17164976da859ed677",
+    "url": "/static/js/2.e0b51945.chunk.js"
+  },
+  {
+    "revision": "6ed9ec5d52cc76550fa29db207ccfd8c",
+    "url": "/static/js/2.e0b51945.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73ccbb36162e82462925",
+    "url": "/static/js/main.5bcf7af7.chunk.js"
+  },
+  {
+    "revision": "9183e1c3bd13d34efcaf",
+    "url": "/static/js/runtime-main.0e79e120.js"
+  },
+  {
+    "revision": "5cc10a08f155f53cf5a52aac0aa5b178",
+    "url": "/static/media/blmcallscript.5cc10a08.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/blmemailresource/precache-manifest.12df361f98c8c6bd02e726fcee808e49.js"
+  "/precache-manifest.82c86207235ac0fb7aa76837baa598b5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/blmemailresource/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
